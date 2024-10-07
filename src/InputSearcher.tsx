654cc7a4685f0d2b203b5d00c156ab68a5abe8ba
Oswaldo <bayourbano@gmail.com>
2024-10-06 22:36:46 -0300

    0.2

--- src/InputSearcher.tsx
+++ src/InputSearcher.tsx
@@ -8,7 +8,14 @@ import OaPagination from './components/oaPagination';
 import { BoxContainer } from './components/boxContainer';
 import { OverlayCard } from './components/overlayCard';
 import PieChartOpenAccess, { ResultPieChartOpenAccess } from "./components/pieOpenAccessChart";
-
+import { Label } from "@/components/ui/label"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
 
 
 
@@ -161,31 +168,79 @@ const InputPage: React.FC = () => {
         <div className='bg-[#f2f0e8]'>
             <div className="bg-[#ffffff] flex flex-col justify-center items-center "><SearcherForm></SearcherForm></div>
             <div className="flex items-center justify-around">
-                <BoxContainer>
-                    <h1>Resultados de la búsqueda</h1>
-                    <p>Buscaste: {searchValue}</p>
 
-                    {resultsLoading ? (
-                        <p>Loading results...</p>
-                    ) : resultsError ? (
-                        <p>Error loading results: {resultsError.message}</p>
-                    ) : (
-                        <QueryResults results={queryResult} type="work" />
-                    )}
-                </BoxContainer>
                 <BoxContainer>
                     {renderBarChart(barchartResults, chartLoading, chartError)}
                 </BoxContainer>
                 {/* <BoxContainer>
                 </BoxContainer>
                */}
-                <OverlayCard>
+                <BoxContainer>
                     {renderPieChart(PiechartResult, PiechartLoading, PiechartError)}
-                </OverlayCard>
+                </BoxContainer>
             </div>
-            <OaPagination page={page} setPage={setPage} />
             {/* {renderPagination()} */}
             {/* </BoxContainer> */}
+            <div >
+                <BoxContainer>
+                <Accordion type="multiple" className="w-full">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>TOPIC</AccordionTrigger>
+                        <AccordionContent>
+                            <div>
+                                <div className="flex items-center space-x-2">
+                                <Checkbox id="terms" />
+                                <label
+                                    htmlFor="terms"
+                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                >
+                                    Article
+                                </label>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="flex items-center space-x-2">
+                                <Checkbox id="terms" />
+                                <label
+                                    htmlFor="terms"
+                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                >
+                                    Book-Chapter
+                                </label>
+                                </div>
+                            </div>
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>INSTITUTION</AccordionTrigger>
+                        <AccordionContent>
+                        Yes. It comes with default styles that matches the other
+                        components&apos; aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>TYPE</AccordionTrigger>
+                        <AccordionContent>
+                        Yes. It&apos;s animated by default, but you can disable it if you
+                        prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                    </Accordion>
+                </BoxContainer>
+                <BoxContainer>
+                        <h1>Resultados de la búsqueda</h1>
+                        <p>Buscaste: {searchValue}</p>
+
+                        {resultsLoading ? (
+                            <p>Loading results...</p>
+                        ) : resultsError ? (
+                            <p>Error loading results: {resultsError.message}</p>
+                        ) : (
+                            <QueryResults results={queryResult} type="work" />
+                        )}
+                </BoxContainer>
+                <OaPagination page={page} setPage={setPage} />
+            </div>
         </div>
     );
 };
